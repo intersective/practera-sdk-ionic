0101b053e8877c242870ae2caa0d30c3cf3ffddd
Kven Ho <kvenho@Kvens-MacBook-Air.local>
2017-06-23 16:06:09 +0800

    Truncate long answer to 150 characters [ISDK-18]

--- src/components/assessments/assessments.component.ts
+++ src/components/assessments/assessments.component.ts
@@ -22,4 +22,12 @@ export class AssessmentsComponent {
     this.assessments = this.assessmentsData;
   }
 
+  truncate(str, length = 150) {
+    if (str.length < length) {
+      return str;
+    }
+
+    return str.slice(0, length - 1) + '...';
+  }
+
 }
