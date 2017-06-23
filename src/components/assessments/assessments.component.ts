f7ffd294ac1532e481c37bd9caa100216bb4e92d
Kven Ho <kvenho@Kvens-MacBook-Air.local>
2017-06-23 17:06:12 +0800

    Create truncate pipe [ISDK-18]

--- src/components/assessments/assessments.component.ts
+++ src/components/assessments/assessments.component.ts
@@ -1,13 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-// import * as _ from 'lodash';
-
-// import { ActivityService } from '../../services/activity.service';
+import { TruncatePipe } from '../../pipes/truncate.pipe';
 
 @Component({
   selector: 'assessments',
-  templateUrl: 'assessments.html'
+  templateUrl: 'assessments.html',
 })
 export class AssessmentsComponent {
   @Input() assessmentsData: any;
@@ -21,13 +19,4 @@ export class AssessmentsComponent {
   ngOnInit() {
     this.assessments = this.assessmentsData;
   }
-
-  truncate(str, length = 150) {
-    if (str.length < length) {
-      return str;
-    }
-
-    return str.slice(0, length - 1) + '...';
-  }
-
 }
