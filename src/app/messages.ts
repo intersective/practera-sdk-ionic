4fad76cc6f59af91138d7de7a7b1aeea1121c9df
Damon Wu <damonwu0605@gmail.com>
2017-06-30 14:23:18 +1000

    [ISDK-31] feature/i18n import and put error message into one single file

---
+++ src/app/messages.ts
@@ -0,0 +1,52 @@
+export const loadingSMSs = {
+  Login: {
+    "login": "Loogging in .."
+  },
+  Loading: {
+    "loading": "Loading .."
+  }
+}
+export const errSMSs = {
+  Activities: {
+    activities: {
+      "loading": "Sorry, loading of the activity process has failed. Please try again later.",
+      "empty": "Sorry, no activity have been returned. Please try again later.",
+      "failed": "Error: Load activity data"
+    },
+    achievements: {
+      "loading": "Sorry, loading of the achievements process has failed. Please try again later.",
+      "empty": "Sorry, no achievements have been returned. Please try again later.",
+      "failed": "Error: Load achievement data"
+    },
+    assessments: {
+      "error": "Sorry, no data have been returned. Please try again later."
+    }
+  },
+  DirectLink: {
+    
+  },
+  Events: {
+    events: {
+      "loading": "Sorry, loading of the events process has failed. Please try again later.",
+      "empty": "Sorry, no events have been returned. Please try again later.",
+      "failed": "Error: Load events data"
+    }
+  },
+  General: {
+    loading: {
+      "load": "Sorry, loading process has failed. Please try again later."
+    },
+    empty: {
+      "empty": "Sorry, no data have been returned. Please try again later."
+    }
+  },
+  Login: {
+
+  },
+  Registration: {
+
+  },
+  ResetPassword: {
+
+  }
+}
