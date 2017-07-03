2edd7235f24836462fde8389db576cac45b66357
Sau Shen Chaw <cshen_581@hotmail.com>
2017-07-03 13:31:42 +0800

    [ISDK-19] integrated enhanced file stack service

--- src/components/questions/file.ts
+++ src/components/questions/file.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { FilestackService, FilestackUpload } from '../../shared/filestack/filestack.service';
 
 @Component({
   selector: 'file-question',
@@ -9,13 +10,28 @@ export class FileQuestionComponent implements OnInit {
   @Input() question;
   @Input() form: FormGroup;
 
-  constructor() {}
+  constructor(
+    private fs: FilestackService
+  ) {}
 
   ngOnInit() {
     console.log(this.form);
   }
 
+  /**
+   * Upload file feature
+   */
   upload(event) {
     console.log(event);
+    this.fs.pick({
+      maxFiles: 5,
+      storeTo1: {
+        location: 's3'
+      }
+    }).then((uploaded: FilestackUpload) => {
+      console.log(uploaded);
+    }, error => {
+      console.log(error);
+    });
   }
 }
