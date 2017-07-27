ba1168f2e0398295a8ee69201adb8821347f175b
Kven Ho <k2command@gmail.com>
2017-07-27 16:53:19 +0800

    Reviewer answer and comment [ISDK-34]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,6 +48,7 @@ import { FileQuestionComponent } from '../components/questions/file';
 import { OneofQuestionComponent } from '../components/questions/oneof';
 import { TextQuestionComponent } from '../components/questions/text';
 import { MultipleQuestionComponent } from '../components/questions/multiple';
+import { FeedbackComponent } from '../components/questions/feedback';
 // pages
 import { AchievementsViewPage } from '../pages/achievements/view/achievements-view.page';
 import { ActivitiesListPage } from '../pages/activities/list/list.page';
@@ -112,6 +113,7 @@ export function HttpLoaderFactory(http: Http) {
     EventsListPage,
     EventsPreviewPage,
     EventsViewPage,
+    FeedbackComponent,
     FileQuestionComponent,
     ForgetPasswordPage,
     GalleryPage,
