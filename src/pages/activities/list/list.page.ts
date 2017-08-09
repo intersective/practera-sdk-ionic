ecb82de7c599f06aefc46674137fd1d03edede6c
Damon Wu <damonwu0605@gmail.com>
2017-08-09 17:00:36 +1000

    feature api new changes WIP

--- src/pages/activities/list/list.page.ts
+++ src/pages/activities/list/list.page.ts
@@ -13,7 +13,9 @@ import * as _ from 'lodash';
 // services
 import { ActivityService } from '../../../services/activity.service';
 import { AchievementService } from '../../../services/achievement.service';
+import { CacheService } from '../../../shared/cache/cache.service';
 import { CharacterService } from '../../../services/character.service';
+import { GameService } from '../../../services/game.service';
 import { SubmissionService } from '../../../services/submission.service';
 // pages
 import { ActivitiesViewPage } from '../view/activities-view.page';
@@ -25,14 +27,13 @@ import { TruncatePipe } from '../../../pipes/truncate.pipe';
  * flag to tell whether should UI popup toast error message at the bottom
  * @type {Boolean}
  */
-const ACTIVATE_TOAST = false;
 
 @Component({
   selector: 'activities-list-page',
   templateUrl: 'list.html'
 })
 export class ActivitiesListPage implements OnInit {
-  public activities = [];
+  public activities: any = [];
   public currentPercentage: any = 0;
   public characterData: any = [];
   public submissionData: any = [];
@@ -40,74 +41,40 @@ export class ActivitiesListPage implements OnInit {
   public percentageValue: number = 0;
   public submissionPoints: number = 0;
   public returnError: boolean = false;
-  // public shiftLang: boolean = false;
   // loading & err message variables
   public activitiesLoadingErr: any = errMessages.General.loading.load;
   public activitiesEmptyDataErr: any = errMessages.Activities.activities.empty;
-
   // Achievements
   private achievements = {
     maxPoint: {},
     obtained: {},
     available: []
   };
-
   constructor(
     public navCtrl: NavController,
     public http: Http,
     public activityService: ActivityService,
     public achievementService: AchievementService,
+    public cacheService: CacheService,
     public characterService: CharacterService,
+    public gameService: GameService,
     public submissionService: SubmissionService,
     public toastCtrl: ToastController,
     public loadingCtrl: LoadingController,
     public modalCtrl: ModalController,
     public translationService: TranslationService
   ) {}
-
-  // shiftLanguageTrial(){
-  //   this.shiftLang = !this.shiftLang;
-  //   this.translationService.isTranslated(this.shiftLang);
-  // }
   ngOnInit() {
-    this.loadingAchievements();
+    this.loadingDashboard();
   }
-
+  // refresher activities
+  doRefresh(e) {
+    this.loadingDashboard();
+    e.complete();
+  }
   // display user achievemnt statistics score points
-  loadingAchievements() {
-    let loadingFailed = this.toastCtrl.create({
-      message: this.activitiesLoadingErr,
-      duration: 4000,
-      position: 'bottom'
-    });
-    let getCharacter = this.characterService.getCharacter();
-    let getSubmission = this.submissionService.getSubmissionsData();
-    Observable.forkJoin([getSubmission, getCharacter])
-              .subscribe(results => {
-                  this.submissionData = results[0];
-                  _.forEach(this.submissionData, element => {
-                    if(element.AssessmentSubmission.status == 'published'){
-                      this.submissionPoints += parseFloat(element.AssessmentSubmission.moderated_score);
-                    }
-                  });
-                  this.percentageValue = (this.submissionPoints/this.submissionData.length)*100;
-                  this.currentPercentage = this.percentageValue.toFixed(2);
-                  console.log("Percent: ", this.currentPercentage); // display as string format
-                  this.characterData = results[1].Character;
-                  this.characterCurrentExperience = this.characterData.experience;
-                  console.log("Experience: ", this.characterCurrentExperience);
-                },
-                err => {
-                  if (ACTIVATE_TOAST) {
-                    loadingFailed.present();
-                  }
-                }
-              );
-  }
-
-  // loading activity list data
-  loadingActivities = () => {
-    let loadingActivities = this.loadingCtrl.create({
+  loadingDashboard() {
+    let loadingData = this.loadingCtrl.create({
       content: 'Loading ..'
     });
     let loadingFailed = this.toastCtrl.create({
@@ -115,39 +82,57 @@ export class ActivitiesListPage implements OnInit {
       duration: 4000,
       position: 'bottom'
     });
-    loadingActivities.present();
-    this.activityService.getList()
-      .subscribe(
-        data => {
-          this.activities = data;
-          if(this.activities.length == 0){
-            this.returnError = true;
-          }
-          loadingActivities.dismiss().then(() => {
-            console.log("Activities: ", this.activities);
-          });
-        },
-        err => {
-          loadingActivities.dismiss().then(() => {
-            if (ACTIVATE_TOAST) {
-              loadingFailed.present();
-            }
-          });
-        }
-      )
+    let getActivities = this.activityService.getList();
+    let getGames = this.gameService.getGames();
+    loadingData.present().then(() => {
+      Observable.forkJoin([getGames, getActivities])
+              .subscribe(
+                results => {
+                    _.map(results[0], (element) => {
+                      // console.log("game id: ", element[0].id);
+                      this.cacheService.setLocal('game_id', element[0].id);
+                    })
+                    this.activities = results[1];
+                    if(this.activities.length == 0){
+                      this.returnError = true;
+                    }
+                    let getCharacter = this.characterService.getCharacter();
+                    let getSubmission = this.submissionService.getSubmissionsData();
+                    Observable.forkJoin([getSubmission, getCharacter])
+                              .subscribe(results => { 
+                                this.submissionData = results[0];
+                                _.forEach(this.submissionData, element => {
+                                  if(element.AssessmentSubmission.status == 'published'){
+                                    this.submissionPoints += parseFloat(element.AssessmentSubmission.moderated_score);
+                                  }
+                                });
+                                loadingData.dismiss().then(() => {
+                                  this.percentageValue = (this.submissionPoints/this.submissionData.length)*100;
+                                  this.currentPercentage = this.percentageValue.toFixed(2);
+                                  // console.log("Percent: ", this.currentPercentage); // display as string format
+                                  this.characterData = results[1].Characters[0];
+                                  this.cacheService.setLocal('character_id', this.characterData.id)
+                                  console.log("character id: ", this.characterData.id);
+                                  this.characterCurrentExperience = this.characterData.experience_points;
+                                  // console.log("Experience: ", this.characterCurrentExperience);
+                                })
+                              },
+                              err => {
+                                loadingData.dismiss().then(() => {
+                                  loadingFailed.present();
+                                });
+                              }
+                            );
+                  
+                },
+                error => {
+                  loadingData.dismiss().then(() => {
+                    loadingFailed.present();
+                  });
+                }
+              );
+    });
   }
-
-  // load activity data
-  ionViewWillEnter() {
-    this.loadingActivities();
-  }
-
-  // refresher activities
-  doRefresh(e) {
-    this.loadingActivities()
-    e.complete();
-  }
-
   // redirect to activity detail page
   goToDetail(activity: any, id: any){
     this.navCtrl.push(ActivitiesViewPage, {
@@ -155,7 +140,6 @@ export class ActivitiesListPage implements OnInit {
       activity: activity
     });
   }
-
   // view the disabled activity popup
   goToPopup(unlock_id: any){
     let disabledActivityPopup = this.modalCtrl.create(ActivityListPopupPage, {unlock_id: unlock_id});
--- src/services/character.service.ts
+++ src/services/character.service.ts
@@ -2,15 +2,20 @@ import { Injectable }    from '@angular/core';
 
 // services
 import { RequestService } from '../shared/request/request.service';
-
+import { CacheService } from '../shared/cache/cache.service';
 @Injectable()
 export class CharacterService {
   public charactersAPIEndpoint = `api/characters.json`;
+  public game_id = this.cache.getLocal('game_id');
+  
   constructor(
-    private request: RequestService
-  ) {}
+    private cache: CacheService,
+    private request: RequestService,
+  ) {
+    console.log("game_id: ", this.game_id);
+  }
 
   getCharacter(){
-    return this.request.get(this.charactersAPIEndpoint);
+    return this.request.get(this.charactersAPIEndpoint + "?game_id=" + this.game_id);
   }
 }
