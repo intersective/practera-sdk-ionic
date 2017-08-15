a192709c780034efcf1bdcab5dd578fd96031079
Kven Ho <k2command@gmail.com>
2017-08-15 17:46:00 +0800

    Change config

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -2,7 +2,7 @@
 // Copy file to config.ts
 export default {
   prefixUrl: 'https://api.practera.com/',
-  appKey: '69ad1e66dc'
+  appKey: '69ad1e66dc',
   filestack: {
     apiKey: 'AO6F4C72uTPGRywaEijdLz'
   },
