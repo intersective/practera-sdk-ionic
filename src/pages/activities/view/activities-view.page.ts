530211963849021601f1034a1ffc0f38a638ad01
Kven Ho <k2command@gmail.com>
2017-08-17 09:56:10 +0800

    [ISDK-22] retrieve submissions from API

--- src/pages/activities/view/activities-view.page.ts
+++ src/pages/activities/view/activities-view.page.ts
@@ -6,6 +6,7 @@ import { ActivitiesViewModalPage } from './activities-view-modal.page';
 // import { AssessmentsPage } from '../../assessments/assessment.page';
 import { AssessmentsPage } from '../../assessments/assessments.page';
 import { ActivityService } from '../../../services/activity.service';
+import { SubmissionService } from '../../../services/submission.service';
 
 import * as _ from 'lodash';
 @Component({
@@ -13,14 +14,15 @@ import * as _ from 'lodash';
 })
 export class ActivitiesViewPage {
   activity: any = {};
-  assessments: Array<any>;
+  assessment: any = {};
   submissions: Array<any> = [];
   constructor(
     private navParams: NavParams,
     private navCtrl: NavController,
     private modalCtrl: ModalController,
     public translationService: TranslationService,
-    private activityService: ActivityService
+    private activityService: ActivityService,
+    private submissionService: SubmissionService,
   ) {}
 
   // @TODO: use simple mock data for assessment first
@@ -32,14 +34,223 @@ export class ActivitiesViewPage {
    * - change template view based on responded data format
    */
   ionViewDidEnter(): void {
-    this.activity = this.activityService.normaliseActivity(this.navParams.get('activity') || {});
-    this.assessments = this.activity.sequences || [];
+    this.activity = this.normaliseActivity(this.navParams.get('activity') || {});
+    this.assessment = this.activity.sequence;
 
-    console.log("Specific Activity Data, ", this.activity);
-    this.activity.badges = [];
-    this.submissions = [];
+    // @TODO: badges images implementation
+    this.activity.badges = [
+      {
+        url: 'http://leevibe.com/images/category_thumbs/video/19.jpg',
+        disabled: true,
+      },
+      {
+        url: 'http://mobileapp.redcross.org.uk/achievements/heart-icon.png',
+        disabled: true,
+      },
+      {
+        url: 'http://americanredcross.3sidedcube.com/media/45334/fire-large.png',
+        disabled: false,
+      },
+    ];
+
+    let submission = [];
+    this.submissionService.getSubmissions({
+      search: { context_id: this.assessment.context_id }
+    }).subscribe(response => {
+      if (response.length > 0) {
+        this.submissions = response;
+      }
+    });
+
+    if (this.activity.Activity.name === 'Workshop-2') {
+      submission.push({
+        title: 'Submission 1',
+        submittedOn: 'Thu Jun 19 2017 17:37:08',
+        status: 'Pending Review'
+      });
+      this.activity.badges.map((badge, index) => {
+        if (index === 1 || index === 0) {
+          badge.disabled = false;
+        } else {
+          badge.disabled = true;
+        }
+      });
+    } else {
+      submission.push({
+        title: 'Submission 1',
+        submittedOn: '',
+        status: 'Do Now'
+      });
+    }
+
+    this.submissions = submission;
     console.log(this.activity);
   }
+
+  /**
+   * normalise activities
+   */
+  private normaliseActivities(activities): Array<any> {
+    let result = [];
+
+    activities.forEach((act, index) => {
+      result[index] = _.merge(act.Activity, {
+        activity: act.Activity,
+        sequences: act.ActivitySequence,
+        Activity: act.Activity,
+        ActivitySequence: act.ActivitySequence,
+        References: act.References
+      });
+    });
+    return result;
+  }
+
+  /*
+    turns:
+    [
+      {
+        "context_id": 25,
+        "Assessment": {
+          "id": 19,
+          "name": "Check-In Workshop 1"
+        }
+      },
+      {
+        "context_id": 26,
+        "Assessment": {
+          "id": 20,
+          "name": "Check-In Workshop 2"
+        }
+      },
+      ...
+    ]
+
+    into:
+    {
+      19: 25,
+      20: 26
+    }
+   */
+  private rebuildReferences(references) {
+    let result = {};
+    references.forEach(ref => {
+      result[ref.Assessment.id] = ref.context_id;
+    });
+    return result;
+  }
+
+  /*
+    @name mergeReferenceToSequence
+
+    turns:
+    [
+      {
+        "id": 52,
+        "activity_id": 22,
+        "model": "Assess.Assessment",
+        "model_id": 19,
+        "order": 0,
+        "is_locked": false,
+        "Assess.Assessment": {
+          "id": 19,
+          "name": "Check-In Workshop 1",
+          "description": "Check in to your first workshop here<br>",
+          "assessment_type": "checkin",
+          "is_live": true,
+          "is_team": false,
+          "score_type": "numeric",
+          "experience_id": 2,
+          "program_id": 4,
+          "deleted": false,
+          "deleted_date": null,
+          "comparison_group_size": 3,
+          "comparison_group_points": 10,
+          "review_period": 72,
+          "review_scope": "assessment",
+          "review_scope_id": null,
+          "created": "2016-02-01 04:45:21.573033",
+          "modified": "2016-10-25 23:54:22",
+          "review_instructions": null,
+          "is_repeatable": false,
+          "num_reviews": null,
+          "review_type": null,
+          "review_role": null,
+          "auto_assign_reviewers": null,
+          "parent_id": null,
+          "auto_publish_reviews": false
+        },
+        "context_id": 25
+      }
+    ]
+
+    into:
+    {
+      "19": {
+        "id": 52,
+        "activity_id": 22,
+        "model": "Assess.Assessment",
+        "model_id": 19,
+        "order": 0,
+        "is_locked": false,
+        "Assess.Assessment": {
+          "id": 19,
+          "name": "Check-In Workshop 1",
+          "description": "Check in to your first workshop here<br>",
+          "assessment_type": "checkin",
+          "is_live": true,
+          "is_team": false,
+          "score_type": "numeric",
+          "experience_id": 2,
+          "program_id": 4,
+          "deleted": false,
+          "deleted_date": null,
+          "comparison_group_size": 3,
+          "comparison_group_points": 10,
+          "review_period": 72,
+          "review_scope": "assessment",
+          "review_scope_id": null,
+          "created": "2016-02-01 04:45:21.573033",
+          "modified": "2016-10-25 23:54:22",
+          "review_instructions": null,
+          "is_repeatable": false,
+          "num_reviews": null,
+          "review_type": null,
+          "review_role": null,
+          "auto_assign_reviewers": null,
+          "parent_id": null,
+          "auto_publish_reviews": false
+        },
+        "context_id": 25
+      }
+    }
+   */
+  private mergeReferenceToSequence(activity) {
+    let refs = this.rebuildReferences(activity.References);
+
+    // @NOTE: first "[0]" sequence is the assessment of an activity
+    let sequence = activity.ActivitySequence[0];
+    // activity.ActivitySequence.forEach(seq => {
+      let modelId = sequence.model_id;
+      sequence.context_id = refs[modelId];
+    // });
+    return sequence;
+  }
+
+  /**
+   * normalise single activity object
+   */
+  private normaliseActivity(activity) {
+    let thisActivity = activity.Activity;
+
+    return _.merge(thisActivity, {
+      activity: activity.Activity,
+      sequence: this.mergeReferenceToSequence(activity),
+      Activity: activity.Activity,
+      ActivitySequence: activity.ActivitySequence,
+      References: activity.References
+    });
+  }
+
   /**
    * @description display activity detail modal page
    */
@@ -48,12 +259,12 @@ export class ActivitiesViewPage {
     detailModal.present();
   }
   /**
-   * @TODO 2017_07_04: ISDK-10, we'll be using first assessment from the list
+   * @name goAssessment
    * @description direct to assessment page of a selected activity
    * @param {Object} activity single activity object from the list of
-   * activities respond from get_activities API
+   *                          activities respond from get_activities API
    */
   goAssessment(activity) {
-    this.navCtrl.push(AssessmentsPage, {activity, assessment: this.assessments[0]});
+    this.navCtrl.push(AssessmentsPage, {activity, assessment: this.assessment});
   }
 }
