e92a199d25087bb6d59e1496780001bfa2845b5b
Kven Ho <k2command@gmail.com>
2017-08-29 14:06:57 +0800

    Setup hour glass for empty characters list [PE-161]

--- src/components/rank/icon.ts
+++ src/components/rank/icon.ts
@@ -6,6 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class RankIconComponent implements OnInit {
   @Input() rank;
+  @Input() isMyRank = false;
 
   constructor() {}
 
