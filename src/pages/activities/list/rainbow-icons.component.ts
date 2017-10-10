fa40e7fdd62963b6ac936d7e5b671ba5fab866c9
chaw <cshen_581@hotmail.com>
2017-10-10 12:45:06 +0800

    [PE-221] removed expression on template string
     wrecker not support expression in string for template string

--- src/pages/activities/list/rainbow-icons.component.ts
+++ src/pages/activities/list/rainbow-icons.component.ts
@@ -14,52 +14,52 @@ const iconColors = [
   selector: 'rainbow-icons',
   template: `<p>
     <span *ngIf="index == 0">
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[0]}': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[0]}': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[0]}': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[0]}': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-6': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-6': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-6': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-6': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
     </span>
 
     <span *ngIf="index == 1">
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[1]}': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[1]}': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[1]}': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[1]}': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-4': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-4': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-4': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-4': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
     </span>
 
     <span *ngIf="index == 2">
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[2]}': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[2]}': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[2]}': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[2]}': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-1': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-1': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-1': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-1': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
     </span>
 
     <span *ngIf="index == 3">
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[3]}': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[3]}': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[3]}': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[3]}': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-2': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-2': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-2': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-2': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
     </span>
 
     <span *ngIf="index == 4">
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[4]}': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[4]}': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[4]}': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[4]}': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-0': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-0': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-0': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-0': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
     </span>
 
     <span *ngIf="index == 5">
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[5]}': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[5]}': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[5]}': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[5]}': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-3': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-3': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-3': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-3': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
     </span>
 
     <span *ngIf="index == 6">
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[6]}': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[6]}': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[6]}': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
-      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o ${iconColors[6]}': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-5': colors[0], 'fa-circle-thin': !colors[0] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-5': colors[1], 'fa-circle-thin': !colors[1] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-5': colors[2], 'fa-circle-thin': !colors[2] }" aria-hidden="true"></i>
+      <i class="fa icon-color-default" [ngClass]="{'fa-check-circle-o icon-color-5': colors[3], 'fa-circle-thin': !colors[3] }" aria-hidden="true"></i>
     </span>
   </p>`
 })
