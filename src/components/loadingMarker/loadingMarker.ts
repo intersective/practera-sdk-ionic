daab3a8bf929170be4cecb4e5a2be58915bdfeb3
chaw <cshen_581@hotmail.com>
2017-10-23 18:10:38 +0800

    [ISDK-82] multiple port over

--- src/components/loadingMarker/loadingMarker.ts
+++ src/components/loadingMarker/loadingMarker.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'loading-marker',
   templateUrl: 'loadingMarker.html'
 })
 export class LoadingMarkerComponent {
-
+  @Input() loading: any;
 }
