1949400a42133a2e2330734c758001ea0e9104dd
Kven Ho <k2command@gmail.com>
2017-11-05 13:18:22 +0800

    Success make fake API call in test [ISDK-87]

--- src/services/milestone.service.spec.ts
+++ src/services/milestone.service.spec.ts
@@ -1,63 +1,46 @@
 import {} from 'jasmine';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { CacheModule } from '../shared/cache/cache.module';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { MilestoneComponentTest } from './milestone.component.test';
 import { MilestoneService } from '../services/milestone.service';
 import { RequestModule } from '../shared/request/request.module';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { default as Configure } from '../configs/config';
-
-// import '../shared/rxjs-operators';
-
-// import { AppModule } from '../app/app.module';
+import { IonicStorageModule } from '@ionic/storage';
 
 describe('Milestone Service test', () => {
 
-  let comp: MilestoneComponentTest;
-  let fixture: ComponentFixture<MilestoneComponentTest>;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        MilestoneComponentTest
+      providers: [
+        { provide: MilestoneService, useClass: MilestoneService }
       ],
       imports: [
-        CacheModule,
-        HttpClientModule,
         HttpClientTestingModule,
+        RequestModule,
+        CacheModule,
         RequestModule.forRoot({
-          appKey: Configure.appKey,
-          prefixUrl: Configure.prefixUrl
+          appKey: '',
+          prefixUrl: ''
+        }),
+        IonicStorageModule.forRoot({
+          name: '__test-app-vault',
+          driverOrder: ['localstorage']
         })
-      ],
-      providers: [
-        { provide: MilestoneService, useClass: MilestoneService },
-        HttpClientTestingBackend,
-        { provide: HttpBackend, useExisting: HttpClientTestingBackend },
-        { provide: HttpTestingController, useExisting: HttpClientTestingBackend },
       ]
     });
-
-    fixture = TestBed.createComponent(MilestoneComponentTest);
-    comp = fixture.componentInstance;
   });
 
-  it('Test getMilestones()', () => {
+  it('should list the fake milestones', () => {
     const milestoneService = TestBed.get(MilestoneService);
     const http = TestBed.get(HttpTestingController);
-    const expectedResponse = [];
-    milestoneService.getMilestones().subscribe((res) => {
-      console.log('res', res)
+
+    const expectedMilestones = {data: []};
+    let actualMilestones = {};
+
+    milestoneService.getMilestones().subscribe((milestones) => {
+      actualMilestones = milestones;
     });
 
-    http.expectOne('/api/milestones.json').flush(expectedResponse);
-    expect(expectedResponse).toEqual(expectedResponse);
-    // fixture.componentInstance.getMilestones()
-    //   .subscribe((res) => {
-    //     console.log('res', res);
-    //   }, (err) => {
-    //     console.log('err', err);
-    //   });
+    http.expectOne('api/milestones.json').flush(expectedMilestones);
+    expect(actualMilestones).toEqual([]);
   });
 });
--- src/shared/cache/cache.service.ts
+++ src/shared/cache/cache.service.ts
@@ -15,7 +15,6 @@ export class CacheService {
     public storage: Storage
   ) {
     storage.ready().then(() => {
-      console.log('Storage loaded.');
       this.read('*', true).then(content => this.bufferZone = content);
     });
 
@@ -107,7 +106,7 @@ export class CacheService {
     this.localStorage[key] = JSON.stringify(value);
   }
 
-  public getLocal(key: string): object {
+  public getLocal(key: string): string | boolean | number | object {
     if (!this.localStorage[key]) {
       return null;
     }
