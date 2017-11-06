184fce3488f97e4572c727f999282ebbd03a7ed3
Kven Ho <k2command@gmail.com>
2017-11-06 11:56:10 +0800

    Update milestone service [ISDK-87]

--- src/services/milestone.service.spec.ts
+++ src/services/milestone.service.spec.ts
@@ -23,24 +23,44 @@ describe('Milestone Service test', () => {
         }),
         IonicStorageModule.forRoot({
           name: '__test-app-vault',
-          driverOrder: ['localstorage']
+          driverOrder: [
+            'localstorage'
+          ]
         })
       ]
     });
   });
 
-  it('should list the fake milestones', () => {
+  it('should list the milestones', () => {
     const milestoneService = TestBed.get(MilestoneService);
     const http = TestBed.get(HttpTestingController);
 
-    const expectedMilestones = {data: []};
-    let actualMilestones = {};
+    const expectedMilestones = {
+      success: true,
+      status: 'success',
+      cache: false,
+      data: [
+        {
+          id: 1241,
+          project_id: 281,
+          name: 'Foundation',
+          description: 'Welcome to Practera and your learning project...',
+          lead_image: 'http://imgurl',
+          duration: 7,
+          order: 0,
+          visibility: 30,
+          delivery: 2,
+          is_locked: false
+        }
+      ]
+    };
+    let actualMilestones = [];
 
     milestoneService.getMilestones().subscribe((milestones) => {
       actualMilestones = milestones;
     });
 
     http.expectOne('api/milestones.json').flush(expectedMilestones);
-    expect(actualMilestones).toEqual([]);
+    expect(actualMilestones).toEqual(expectedMilestones.data);
   });
 });
