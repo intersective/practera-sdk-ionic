99183aacc566dd28d15fa522463bf38ec37fefea
Dameng <damon@intersective.com>
2017-11-13 11:11:18 +1100

    Using dynamic URL endpoint for user portfolio

--- src/pages/activities/list/list.page.ts
+++ src/pages/activities/list/list.page.ts
@@ -148,9 +148,9 @@ export class ActivitiesListPage {
     if(this.email && this.program_id){
       this.program_id = this.cacheService.getLocal('program_id');
       this.email = this.cacheService.getLocal('email');
-      this.viewPortfolioLink = `https://practera.com/assess/assessments/portfolio/${this.program_id}/${this.email}`;
+      this.viewPortfolioLink = `${this.portfolio_domain}/${this.program_id}/${this.email}`;
     }else {
-      this.viewPortfolioLink = `https://practera.com/assess/assessments/portfolio/1/test@test.com`;
+      this.viewPortfolioLink = `${this.portfolio_domain}/1/test@test.com`;
     }
   }
   ionViewWillEnter(){
